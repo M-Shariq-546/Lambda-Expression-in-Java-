8e453764d027059a4b169310a8850d678348fa7f
Muhammad Shariq Shafiq <107953192+M-Shariq-546@users.noreply.github.com>
2023-08-28 18:25:49 +0500

    Lambda Returned Code in java

---
+++ LambdaReturned.java
@@ -0,0 +1,15 @@
+@FunctionalInterface
+interface My{
+    String Show(String name);
+}
+
+class ReturnedString
+{
+    public static void main(String [] args)
+    {
+        My s = (String name) -> name;
+                
+        String myself = s.Show("Muhammad Shariq Shafiq");
+        System.out.println("Hello MySelf "+ myself);
+    }
+} 
